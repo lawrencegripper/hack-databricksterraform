5ad9a6614c0978450ce96caa802c159ae4889cba
Lawrence Gripper <info@grippers.co.uk>
2020-04-27 19:07:50 +0000

    More psake tweaks

--- make.ps1
+++ make.ps1
@@ -1,15 +1,15 @@
 task default -depends Test
 
 task "test" -depends "InstallRequirements" -Description "Run unit tests for project" {
-    & ./scripts/cluster.tests.ps1
+    exec { & ./scripts/cluster.tests.ps1 }
 }
 
 task "integrationTest" -depends "InstallRequirements" -Description "Create real cluster in Azure and assert correctly created" {
-    & ./scripts/integration_tests.ps1
+    exec { & ./scripts/integration_tests.ps1 }
 }
 
 task "installRequirements" {
-    & ./scripts/install_requirements.ps1
+    exec { & ./scripts/install_requirements.ps1 }
 }
 
 task "clean" {
@@ -28,27 +28,26 @@ task "fixup" {
 
 task "checks" -depends "installRequirements", "clean" {
     Write-Host  ">> Powershell Script Analyzer"
-    Invoke-ScriptAnalyzer -Path ./scripts -Recurse -Settings PSGallery -EnableExit
+    exec { Invoke-ScriptAnalyzer -Path ./scripts -Recurse -Settings PSGallery -EnableExit }
 
     Write-Host  ">> Terraform verion"
-    terraform -version 
+    exec { terraform -version }
 
     Write-Host  ">> Terraform Format (if this fails use 'terraform fmt' command to resolve"
-    terraform fmt -recursive -diff -check
+    exec { terraform fmt -recursive -diff -check }
 
     Write-Host  ">> tflint"
-    tflint
+    exec { tflint }
 
     Write-Host  ">> Terraform init"
-    terraform init
+    exec { terraform init }
 
     Write-Host  ">> Terraform validate"
-    terraform validate
+    exec { terraform validate }
 }
 
 task "ci" {
-    docker build -f ./.devcontainer/Dockerfile ./.devcontainer -t localdevcontainer:latest
-
-    docker run -v ${PWD}:${PWD} -v /var/run/docker.sock:/var/run/docker.sock --workdir "${PWD}" --entrypoint /bin/bash -t localdevcontainer:latest -c "pwsh -c 'Invoke-psake ./make.ps1 ci'"
+    exec { & docker build -f ./.devcontainer/Dockerfile ./.devcontainer -t localdevcontainer:latest }
+    exec { & docker run -v ${PWD}:${PWD} -v /var/run/docker.sock:/var/run/docker.sock --workdir "${PWD}" --entrypoint /bin/bash -t localdevcontainer:latest -c "pwsh -c 'Invoke-psake ./make.ps1 ci'" }
 
 }
